0a4806160dcabc9695bc3a956926c43316ba55c9
kyeong98 <90947285+kyeong98@users.noreply.github.com>
2023-06-08 22:21:05 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+class Calculator:
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @staticmethod
+    def subtract(x, y):
+        return x - y
+
+    @staticmethod
+    def multiply(x, y):
+        return x * y
+
+    @staticmethod
+    def divide(x, y):
+        if y != 0:
+            return x / y
+        else:
+            return "Error: Division by zero"
+
+    @staticmethod
+    def power(x, y):
+        return x ** y
